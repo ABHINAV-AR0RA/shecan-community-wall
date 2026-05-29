8f319665bc2d67cf3210479352b5351d1be51454
ABHINAV-AR0RA <abhinavarora283@gmail.com>
2026-05-29 21:04:32 +0530

    api fix

--- client/src/api/messages.js
+++ client/src/api/messages.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://shecan-api.onrender.com/api', 
+  baseURL: 'https://shecan-api.onrender.com/api/messages', 
   headers: {
     'Content-Type': 'application/json',
   },
